2ddc29e9bffaa6e9b3a792f423c794ac280851d3
glorious73 <amjadmh73@gmail.com>
2022-12-05 18:43:29 +0300

    working right to left

--- src/js/Component/UI/Navbar/NavbarComponent.js
+++ src/js/Component/UI/Navbar/NavbarComponent.js
@@ -11,32 +11,31 @@ function markupTemplate() {
         </style>
         <header>
             <div class="nav nav-box-shadow">
-            <input type="checkbox" id="nav-check">
-            <div class="nav-header">
-                <div class="nav-title">
-                    <!--Googled Randomly-->
-                    <img class="navbar-brand-img" src="assets/img/masarak_logo.png" alt="Logo" />
+                <input type="checkbox" id="nav-check">
+                <div class="nav-header">
+                    <div class="nav-title">
+                        <img class="navbar-brand-img" src="assets/img/masarak_logo.png" alt="Logo" />
+                    </div>
+                </div>
+                <div class="nav-btn">
+                    <label for="nav-check">
+                        <span></span>
+                        <span></span>  
+                        <span></span>
+                    </label>
+                </div>
+                <div class="nav-items">
+                    <a href="#">Home</a>
+                    <a href="#">Services</a>
+                    <a href="#">Contact</a>
+                    <input type="checkbox" id="nav-dropdown">
+                    <label for="nav-dropdown">Admin+</label>
+                    <ul class="nav-dropdown-list">
+                        <li><a href="#">Dashbord</a></li>
+                        <li><a href="#">Logout</a></li>
+                    </ul>
                 </div>
             </div>
-            <div class="nav-btn">
-                <label for="nav-check">
-                    <span></span>
-                    <span></span>  
-                    <span></span>
-                </label>
-            </div>
-            <div class="nav-items">
-                <a href="#">Home</a>
-                <a href="#">Services</a>
-                <a href="#">Contact</a>
-                <input type="checkbox" id="nav-dropdown">
-                <label for="nav-dropdown">Admin+</label>
-                <ul class="nav-dropdown-list">
-                    <li><a href="#">Dashbord</a></li>
-                    <li><a href="#">Logout</a></li>
-                </ul>
-            </div>
-        </div>
         </header>
     `;
     return template;
